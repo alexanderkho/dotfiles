be063d247e51f969d8d86bf00d60c8ed9787d64f
Alex Ho <alexanderkho92@gmail.com>
2024-03-08 18:21:23 -0700

    add nvim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  { "rebelot/kanagawa.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "tokyonight",
+      colorscheme = "kanagawa",
+    },
+  },
+}
