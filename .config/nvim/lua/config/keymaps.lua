c804458d76f8d9ea3226b933c4416b9617bdf1cf
Alex Ho <alexanderkho92@gmail.com>
2024-04-07 11:25:18 -0600

    tmux + nvim keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- exit insert mode with jk
+vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
+
+-- exit all
+vim.keymap.set("c", "Q", "%bd|q|y", { noremap = true })
