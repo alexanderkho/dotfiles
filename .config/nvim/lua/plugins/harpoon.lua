21b7a9e2f5226e016fcbef70e2b23c5502db2ffc
Alex Ho <alexanderkho92@gmail.com>
2024-04-26 20:44:49 -0600

    setup harpoon

---
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+
+      -- REQUIRED
+      harpoon:setup()
+      -- REQUIRED
+
+      vim.keymap.set("n", "<leader>a", function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set("n", "<C-e>", function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      vim.keymap.set("n", "<leader>a", function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set("n", "<leader>s", function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set("n", "<leader>d", function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set("n", "<leader>f", function()
+        harpoon:list():select(4)
+      end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      -- TODO: these don't seem to work currently
+      vim.keymap.set("n", "<C-S-P>", function()
+        harpoon:list():prev()
+      end)
+      vim.keymap.set("n", "<C-S-N>", function()
+        harpoon:list():next()
+      end)
+    end,
+  },
+}
